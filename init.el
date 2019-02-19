b1d01d96bb7923787987d44a8d7f41b88cf3b60b
Chizi123 <joelgrun@gmail.com>
2019-02-19 19:23:53 +1100

    Added reftex and some minor changes

--- init.el
+++ init.el
@@ -37,10 +37,12 @@
  '(ede-project-directories (quote ("c:/Users/joelg/.emacs.d")))
  '(package-selected-packages
    (quote
-    (gnuplot-mode gnuplot matlab-mode diminish keyfreq zenburn-theme yasnippet x86-lookup workgroups2 which-key volatile-highlights use-package undo-tree srefactor smartparens racket-mode popwin plantuml-mode pdf-tools org-bullets org nyan-mode magit latex-preview-pane helm-projectile ggtags flycheck-pos-tip flycheck-clang-analyzer company-math company-c-headers clean-aindent-mode auto-package-update auctex))))
+    (minesweeper speed-type spray fireplace powershell company-auctex company-bibtex company-irony-c-headers company-reftex gnuplot-mode gnuplot matlab-mode diminish keyfreq zenburn-theme yasnippet x86-lookup workgroups2 which-key volatile-highlights use-package undo-tree srefactor smartparens racket-mode popwin plantuml-mode pdf-tools org-bullets org nyan-mode magit latex-preview-pane helm-projectile ggtags flycheck-pos-tip flycheck-clang-analyzer company-math company-c-headers clean-aindent-mode auto-package-update auctex))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'narrow-to-page 'disabled nil)
+(put 'narrow-to-region 'disabled nil)
