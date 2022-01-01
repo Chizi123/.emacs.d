4ebb4dc316ad135c94c94755e7bf8dcd1403b9e4
Joel Grunbaum <joelgrun@gmail.com>
2022-01-01 18:23:09 +1100

    Move spell checking to use-package

--- init.el
+++ init.el
@@ -5,8 +5,8 @@
 ;; Repos
 (require 'package)
 (setq package-archives '(("gnu"   . "https://elpa.gnu.org/packages/")
-			 ("nongnu"  . "https://elpa.nongnu.org/nongnu/")
-			 ("melpa" . "https://melpa.org/packages/")))
+			             ("nongnu"  . "https://elpa.nongnu.org/nongnu/")
+			             ("melpa" . "https://melpa.org/packages/")))
 (package-initialize)
 
 ;; use-package
